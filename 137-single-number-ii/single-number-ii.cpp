930bbc5dc83320b60b0181ae7975b6a0f4461258
Anshika411 <singlaanshika411@gmail.com>
2026-07-16 23:51:33 +0530

    Time: 5 ms (5.04%) | Memory: 14 MB (10.70%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        map<int,int>mp;
+        for(int i : nums){
+            mp[i]++;
+        }
+        for(auto it:mp){
+            if(it.second ==1 ){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
