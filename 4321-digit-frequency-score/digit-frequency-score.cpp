1ec8f45713620808d4aa2148f3dba6d9003ac75d
Anshika <singlaanshika411@gmail.com>
2026-07-25 02:04:22 +0530

    Time: 1 ms (25.00%) | Memory: 8.4 MB (95.18%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int sum = 0;
+        while(n>0){
+            int last_digit = n%10;
+            n /=10;
+            sum+= last_digit;
+
+        }
+        return sum;
+    }
+};
