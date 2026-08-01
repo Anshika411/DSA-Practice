8738b5f1d4dfe72d036196d75d5c90c37f75f509
Anshika <singlaanshika411@gmail.com>
2026-08-01 17:43:05 +0530

    Time: 11 ms (39.46%) | Memory: 44.9 MB (48.87%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -3,12 +3,12 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int cnt = 0;
         int j = 0;
-        for (int i = 0; i<s.size() && j<g.size(); i++) {
-            if (g[j]<=s[i]){
+        int cnt = 0;
+        for(int i = 0; i<g.size() && j<s.size(); j++){
+            if(g[i]<=s[j]){
+                i++;
                 cnt++;
-                j++;
             }
         }
         return cnt;
