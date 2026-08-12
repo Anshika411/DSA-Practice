c7ea18728d6efefaec20e059486093331638a29d
Anshika <singlaanshika411@gmail.com>
2026-08-12 22:54:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.65%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     double myPow(double x, int n) {
         long long N = n;
-
         if (N < 0) {
             x = 1 / x;
             N = -N;
@@ -14,11 +13,9 @@ public:
             if (N % 2 == 1) {
                 ans *= x;
             }
-
             x *= x;
             N /= 2;
         }
-
         return ans;
     }
 };
