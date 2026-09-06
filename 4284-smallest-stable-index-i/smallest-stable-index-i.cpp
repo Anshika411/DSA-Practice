021d5e1a0d2d48730469d460500bffb121db5f51
Anshika <singlaanshika411@gmail.com>
2026-09-07 03:17:05 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (80.94%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,25 +2,13 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        
-        vector<int> sufMin(n);
-        sufMin[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; --i) {
-            sufMin[i] = min(sufMin[i + 1], nums[i]);
-        }
-        
-        int prefMax = 0;
-        
-        // Iterate through each index to find the first stable index
-        for (int i = 0; i < n; ++i) {
-            prefMax = max(prefMax, nums[i]);
-            
-            // Instability score = max(nums[0..i]) - min(nums[i..n-1])
-            if (prefMax - sufMin[i] <= k) {
+        for (int i = 0; i < n; i++) {
+            int high = *max_element(nums.begin(), nums.begin() + i);
+            int low = *min_element(nums.begin() + i, nums.end());
+            int score = high - low;
+            if (score <= k)
                 return i;
-            }
         }
-        
         return -1;
     }
 };
