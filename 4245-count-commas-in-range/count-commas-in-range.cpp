7384661b89877f156ce7ef7a67f894e14f7422fc
Anshika <singlaanshika411@gmail.com>
2026-09-09 04:53:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.44%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int cnt = 0;
+        if(n<1000){
+            return 0;
+        } else {
+            cnt+= (n-999);
+        }
+        return cnt;
+    }
+};
